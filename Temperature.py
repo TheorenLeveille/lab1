3f29d9c3da824de321a501a7b2299f8113532e29
TheoLeveille <tleveille@my.bcit.ca>
2019-01-15 15:18:19 -0800

    initial commit

---
+++ Temperature.py
@@ -0,0 +1,11 @@
+# python program to convert celsius into fahrenheit
+
+# assigning value to celsius
+celsius = 10
+
+# calculating fahrenheit
+fahrenheit = celsius * (9 / 5) + 32
+
+# printing the result
+print("Celsius:", celsius)
+print("Fahrenheit:", fahrenheit)
---
+++ poem.py
@@ -0,0 +1,17 @@
+# Printing poem line by line
+
+# Assigning values
+poem_line1 = "Roses are red"
+poem_line2 = "Violets are blue"
+poem_line3 = "Sugar is Sweet"
+poem_line4 = "But I have commitment issues"
+poem_line5 = "So I\'d rather just be friends"
+poem_line6 = "At this point in our relationship."
+
+# printing poem lines
+print(poem_line1)
+print(poem_line2)
+print(poem_line3)
+print(poem_line4)
+print(poem_line5)
+print(poem_line6)
